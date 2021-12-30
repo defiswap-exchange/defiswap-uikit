55ffb6356ebcf55b7ad1cb8f72a3ec7242c936b6
IWANDO <ceo@iwando.com>
2021-12-30 13:18:50 +0100

    Replaced SVG files

--- src/components/Svg/Icons/NoProfileAvatar.tsx
+++ src/components/Svg/Icons/NoProfileAvatar.tsx
@@ -9,23 +9,45 @@ const Icon: React.FC<SvgProps> = (props) => {
   const secondaryColor = theme.isDark ? "#666171" : "#bdc2c4";
 
   return (
-    <Svg viewBox="0 0 32 32" {...props}>
-      <path d="M32 16c0 8.837-7.163 16-16 16S0 24.837 0 16 7.163 0 16 0s16 7.163 16 16z" fill={primaryColor} />
-      <mask id="A" mask-type="alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="32" height="32">
-        <path d="M32 16c0 8.837-7.163 16-16 16S0 24.837 0 16 7.163 0 16 0s16 7.163 16 16z" fill="#c4c4c4" />
+    <Svg viewBox="0 0 48 48" {...props}>
+      <path
+        fill="#E9EAEB"
+        d="M47.4,24C47.4,37,36.9,47.4,24,47.4S0.6,37,0.6,24S11.1,0.7,24,0.7S47.4,11.1,47.4,24z"
+      />
+      <defs>
+        <filter
+          id="Adobe_OpacityMaskFilter"
+          filterUnits="userSpaceOnUse"
+          x="7.5"
+          y="10.3"
+          width="33.1"
+          height="37.2"
+        >
+          <feColorMatrix
+            type="matrix"
+            values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 1 0"
+          />
+        </filter>
+      </defs>
+      <mask
+        maskUnits="userSpaceOnUse"
+        x="7.5"
+        y="10.3"
+        width="33.1"
+        height="37.2"
+        id="A_2_"
+      >
+        <path
+          fill="#C4C4C4"
+          d="M47.4,24C47.4,37,36.9,47.4,24,47.4S0.6,37,0.6,24S11.1,0.7,24,0.7S47.4,11.1,47.4,24z"
+        />
       </mask>
-      <g mask="url(#A)">
+      <g style={{ mask: "url(#A_2_)" }}>
         <path
-          d="M25.128 16.436c0 3.115-4.133 5.641-9.231 5.641s-9.231-2.526-9.231-5.641V15h18.461v1.436zm2.205 13.806c0-3.815-5.074-6.908-11.333-6.908S4.667 26.426 4.667 30.242V32h22.667v-1.759z"
-          fill={secondaryColor}
+          fill="#BDC2C4"
+          d="M40.6,44.9c0-5.6-7.4-10.1-16.6-10.1S7.5,39.3,7.5,44.9v2.6h33.1L40.6,44.9L40.6,44.9z M36.3,21.6
+		c0,6.3-5.6,11.4-12.4,11.4s-12.4-5.1-12.4-11.4S17,10.3,23.9,10.3S36.3,15.4,36.3,21.6z"
         />
-        <path
-          fillRule="evenodd"
-          d="M10.234 5.601C9.942 4.264 10.96 3 12.328 3c1.184 0 2.143.959 2.143 2.143v3.873l1.427-.067c.589 0 1.166.034 1.724.098V5.143c0-1.184.959-2.143 2.143-2.143 1.368 0 2.386 1.264 2.093 2.601l-.931 4.258c2.529 1.006 4.201 2.749 4.201 4.731 0 3.115-4.133 5.641-9.231 5.641s-9.231-2.526-9.231-5.641c0-2.053 1.794-3.849 4.476-4.836l-.908-4.153z"
-          fill={secondaryColor}
-        />
-        <ellipse cx="12.308" cy="14.846" rx="1.026" ry="1.538" fill={primaryColor} />
-        <ellipse cx="19.385" cy="14.846" rx="1.026" ry="1.538" fill={primaryColor} />
       </g>
     </Svg>
   );
--- src/components/Svg/Icons/TrophyFill.tsx
+++ src/components/Svg/Icons/TrophyFill.tsx
@@ -1,15 +1,82 @@
-import * as React from "react";
+import React from "react";
+import { useTheme } from "styled-components";
 import Svg from "../Svg";
 import { SvgProps } from "../types";
 
-const Icon: React.FC<SvgProps> = (props) => (
-  <Svg viewBox="0 0 18 20" {...props}>
+const Icon: React.FC<SvgProps> = (props) => {
+  const theme = useTheme();
+  const fillColor = theme.isDark ? "#280D5F" : "#FFFFFF";
+  return (
+  <Svg viewBox="0 0 48 53" {...props}>
     <path
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M5.95.446a1.5 1.5 0 00-1.11 1.808c.08.327.457 1.213.877 2.15H2c-1.1 0-2 .9-2 2v1c0 .319.03.63.087.931.401 2.111 2.142 3.738 4.303 4.009A5.01 5.01 0 008 15.304v2.1H5a1 1 0 100 2h8.006a1 1 0 00-.006-2h-3v-2.1a5.013 5.013 0 003.61-2.96c.309-.039.609-.105.898-.197C16.53 11.507 18 9.635 18 7.404v-1c0-1.1-.9-2-2-2h-3.718c.42-.937.798-1.823.877-2.15a1.5 1.5 0 00-2.918-.7l-.683 2.85H8.442l-.684-2.85A1.5 1.5 0 005.949.446zM16 7.404c0 1.3-.84 2.4-2 2.82v-3.82h2v1zm-12 2.82c-1.16-.42-2-1.52-2-2.82v-1h2v3.82z"
-    />
+        d="M17.1 33.3c-5.3-7.2-7.6-20.1-7.6-20.1l14.4-7.8 14.4 7.8s-2.1 13.1-7 20.3l-14.2-.2z"
+        fill="#796AAE"
+      ></path>
+      <path
+        d="M20.8 33.7c-.2 0-.2 0-.4-.2-2.5-2.1-4.9-6-6.6-11.3-1.4-3.9-2-7-2-7.2-.2-.4 0-.8.4-.8s.6.2.8.6c0 .2 2.3 12.9 8.2 18 .2.2.2.6 0 .8-.2-.1-.2.1-.4.1z"
+        opacity="0.38" fill="#664610" enable-background="new"
+      ></path>
+      <path
+        d="M35.5 11.4c-.8 3.7-2.5 10-5.1 15-1 1.8-2.7 2.9-4.7 2.9h-11c.8 1.4 1.4 2.7 2.3 3.9h14.4c4.9-7.2 7-20.3 7-20.3l-2.9-1.5z"
+        opacity="0.3" fill="#664610" enable-background="new"
+      ></path>
+      <path
+        d="M38 45.8H10V36c0-1.4 1.2-2.7 2.5-2.7h22.6c1.4 0 2.5 1.2 2.5 2.7l.4 9.8z"
+        fill="#664610"
+      ></path>
+      <path d="M10 41.3H37.9V45.599999999999994H10z" fill="#57371D"></path>
+      <path
+        d="M12.2 44.2c-.4 0-.6-.2-.6-.6V37c0-.4.2-.6.6-.6s.6.2.6.6v6.4c.2.4-.2.8-.6.8z"
+        fill="#57371D"
+      ></path>
+      <path
+        d="M30.5 38.9H17.8c-.8 0-1.2-.6-1.2-1.2v-.2c0-.8.6-1.2 1.2-1.2h12.9c.8 0 1.2.6 1.2 1.2v.2c-.1.6-.6 1.2-1.4 1.2z"
+        fill="#FFF5CC"
+      ></path>
+      <g>
+        <path
+          d="M27 20.6L23.9 23.5 20.8 20.6 19 22.3 23.9 27.2 25.8 25.5 25.8 25.5 28.8 22.3z"
+          fill={fillColor}
+        ></path>
+        <path
+          d="M18.2 19.8L17.5 20.8 18.2 21.6 19.2 20.8z"
+          fill={fillColor}
+        ></path>
+        <path
+          d="M16.9 18.2L15.9 19.2 16.8 20 17.8 19.2z"
+          fill={fillColor}
+        ></path>
+        <path
+          d="M19.2 17.7L18.2 16.9 17.5 17.7 18.2 18.4z"
+          fill={fillColor}
+        ></path>
+        <path d="M19.8 20L20.8 19.2 19.8 18.2 19 19.2z" fill={fillColor}></path>
+        <path
+          d="M28.6 20.8L29.4 21.6 30.3 20.8 29.4 19.8z"
+          fill={fillColor}
+        ></path>
+        <path d="M27 19.2L28 20 28.8 19.2 28 18.4z" fill={fillColor}></path>
+        <path
+          d="M29.4 18.6L30.3 17.7 29.4 16.9 28.6 17.7z"
+          fill={fillColor}
+        ></path>
+        <path
+          d="M30.9 18.4L30.1 19.2 30.9 20 31.9 19.2z"
+          fill={fillColor}
+        ></path>
+        <path
+          d="M27 17.9L23.9 14.7 20.8 17.9 19 16.1 23.9 11.2 25.8 13 25.8 13 28.8 16.1z"
+          fill={fillColor}
+        ></path>
+      </g>
+      <g>
+        <path
+          d="M21.8 18.8l1-.4v2h1.6c.4 0 .6-.2.8-.6.2-.2.2-.4.2-.8s-.2-.8-.4-1c-.2-.2-.4-.2-.6-.2H22l.4-.8h2.3c.6 0 1 .2 1.4.8.4.4.4.8.4 1.4s-.2 1-.4 1.4c-.4.4-.8.6-1.2.6h-2.5l-.6-2.4z"
+          fill={fillColor}
+        ></path>
+      </g>
   </Svg>
-);
+ );
+};
 
 export default Icon;
